78170df6f86a836c042c7921b39b069203e4bd09
Tim Park <timfpark@gmail.com>
2021-10-11 10:29:24 -0700

    Add mocked NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import React from "react";
+import logo from "./logo.svg";
+
+import "./App.css";
+
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Platform Portal</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#clusters">Clusters</Nav.Link>
+                            <Nav.Link href="#link">Workspaces</Nav.Link>
+                            <Nav.Link href="#link">Applications</Nav.Link>
+                        </Nav>
+                        <Nav className="justify-content-end">
+                            <NavDropdown
+                                title="Tim Park"
+                                id="basic-nav-dropdown"
+                            >
+                                <NavDropdown.Item href="#action/3.4">
+                                    Log Out
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
 }
 
 export default App;
